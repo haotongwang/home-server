39ea01a2fb5c01e1240e82e1f69a10cc708eb7ae
Nick Wang <nicholas.wang.h@gmail.com>
2020-01-11 01:29:08 +1100

    Refactor index.js to use Express

--- index.js
+++ index.js
@@ -1,88 +1,37 @@
 'use strict';
 
-const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
+const { JSDOM } = require('jsdom');
+const express = require('express');
+
+const app = express();
+
+/* ------------------------------- functions ------------------------------- */
+
+/**
+ * Set a directory to serve static files
+ * @param {String} dirName Name of directory
+ */
+function setStatic(dirName) {
+	app.use(`/${dirName}`, express.static(dirName));
+	app.get(`/${dirName}`, (req, res) => {
+		res.sendFile(path.join(__dirname, dirName, `${dirName}.html`));
+	});
+}
+
+/* ------------------------------ static files ------------------------------ */
+
+setStatic('file');
+
+/* ---------------------------------- 404 ---------------------------------- */
+
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
 
 /* --------------------------------- server --------------------------------- */
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-	let filePath = '.' + decodeURI(req.url);
-	if (filePath == './') {
-		filePath = './index.html';
-	} else if (path.extname(req.url) === '') {
-		const [folder] = filePath.match('[^\\/]+(?=\\/$)|[^\\/]+$');
-		filePath = path.join(filePath, folder + '.html');
-	}
-
-	console.log(filePath);
-
-	const extname = path.extname(filePath);
-	let contentType = 'document';
-	switch (extname) {
-		case '.html':
-			contentType = 'text/html';
-			break;
-		case '.txt':
-			contentType = 'text/plain';
-			break;
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-		case '.json':
-			contentType = 'application/json';
-			break;
-		case '.png':
-			contentType = 'image/png';
-			break;
-		case '.jpg':
-			contentType = 'image/jpg';
-			break;
-		case '.wav':
-			contentType = 'audio/wav';
-			break;
-		case '.pdf':
-			contentType = 'application/pdf';
-			break;
-		case '.mp4':
-			contentType = 'video/mp4';
-			break;
-	}
-
-	fs.readFile(filePath, function(err, data) {
-		if (err) {
-			if (err.code == 'ENOENT') {
-				fs.readFile('./404.html', function(err, data) {
-					res.writeHead(200, { 'Content-Type': contentType });
-					res.end(data, 'utf-8');
-				});
-			} else {
-				res.writeHead(500);
-				res.end(`Unkown error (${err.code})`);
-			}
-		} else {
-			res.writeHead(200, { 'Content-Type': contentType });
-			res.end(data);
-		}
-	});
-}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-/* -------------------------------- terminal -------------------------------- */
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-rl.on('line', (input) => {
-	if (input === '.close' || input === '.quit' || input === '.exit') {
-		server.close();
-		process.exit();
-	};
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
