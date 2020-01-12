7160ff5b795da180846f3660d3dab64e8449e502
Nick Wang <nicholas.wang.h@gmail.com>
2020-01-13 00:17:58 +1100

    Add upload files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const { JSDOM } = require('jsdom');
 const express = require('express');
+const Formidable = require('formidable');
+const util = require('util');
 
 const app = express();
 
@@ -41,6 +43,10 @@ function generateHTML(dirPath, title=null) {
 		ul.appendChild(li);
 	});
 
+	const div = document.createElement('div');
+	div.innerHTML=`<form action="/upload"enctype="multipart/form-data"method="POST"><input type="file"name="upload"multiple="multiple"><input type="submit"value="Upload"></form>`;
+	document.body.appendChild(div);
+
 	return document.documentElement.outerHTML;
 }
 
@@ -51,15 +57,35 @@ app.get(['/', '/index.html'], (req, res) => {
 	res.send(generateHTML(path.join(__dirname, 'public'), 'Home Server'));
 });
 
+// upload page
+app.get('/upload.html', (req, res) => {
+	res.sendFile(path.join(__dirname, 'upload.html'));
+});
+
+// upload POST method
+app.post('/upload', (req, res) => {
+	const form = new Formidable();
+	form.uploadDir = path.join(__dirname, 'temp');
+	form.keepExtensions = true;
+	form.multiples = true;
+
+	form.parse(req, (err, fields, files) => {
+		if (err) console.error(err);
+		const { upload } = files;
+
+		upload.forEach((file) => {
+			fs.renameSync(file.path, path.join(path.dirname(file.path), file.name));
+		});
+
+		res.end(util.inspect(req));
+	});
+});
+
 // generalised page generation
 app.get('*', (req, res, next) => {
 	const abs = path.join(__dirname, 'public', req.url);
-	if (fs.existsSync(abs)) {
-		if (fs.statSync(abs).isDirectory()) {
-			res.send(generateHTML(abs));
-		} else {
-			res.sendFile(abs);
-		}
+	if (fs.existsSync(abs) && fs.statSync(abs).isDirectory()) {
+		res.send(generateHTML(abs));
 	} else {
 		next();
 	}
