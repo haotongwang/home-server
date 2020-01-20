c3e32198d7db596bbae3834af470e126512fc230
Nick Wang <nicholas.wang.h@gmail.com>
2020-01-21 10:09:58 +1100

    Move functions to module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const util = require('util'); // eslint-disable-line no-unused-vars
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require('jsdom');
+// const { JSDOM } = require('jsdom');
 const express = require('express');
 // const Formidable = require('formidable');
 // const url = require('url');
@@ -52,43 +52,13 @@ const global = (() => {
 
 /* ------------------------------- functions ------------------------------- */
 
-/**
- * Generate a stringified HTML file listing the contents of the directory
- * @param {String} dirPath Path to directory
- * @param {String} title Title of page
- * @return {String} Stringified HTML
- */
-function generateHTML(dirPath, title=null) {
-	title = title || path.basename(dirPath);
-
-	const { document } = new JSDOM().window;
-	document.title = title;
-
-	const h1 = document.createElement('h1');
-	h1.innerHTML = title;
-	document.body.appendChild(h1);
-
-	const ul = document.createElement('ul');
-	document.body.appendChild(ul);
-
-	fs.readdirSync(dirPath).forEach((d) => {
-		const li = document.createElement('li');
-		li.innerHTML = `<a href="${d}">${d}</a>`;
-		ul.appendChild(li);
-	});
-
-	const div = document.createElement('div');
-	div.innerHTML = `<form action="/upload"enctype="multipart/form-data"method="POST"><input type="file"name="upload"multiple="multiple"oninput="if(this.value)submit.disabled=false;else submit.disabled=true;"><input type="submit"value="Upload"id="submit"disabled></form>`;
-	document.body.appendChild(div);
-
-	return document.documentElement.outerHTML;
-}
+const htmlGen = require('./lib/htmlGen');
 
 /* ------------------------------ core routes ------------------------------ */
 
 // home page
 app.get(['/', '/index.html'], (req, res) => {
-	res.send(generateHTML(global.serveDirectory, 'Home Server'));
+	res.send(htmlGen.directory(global.serveDirectory, 'Home Server'));
 });
 
 /* ----------------------------- import routes ----------------------------- */
@@ -105,7 +75,7 @@ app.use(require('./routes/set'));
 app.get('*', (req, res, next) => {
 	const abs = path.join(global.serveDirectory, req.url);
 	if (fs.existsSync(abs) && fs.statSync(abs).isDirectory()) {
-		res.send(generateHTML(abs));
+		res.send(htmlGen.directory(abs));
 	} else {
 		next();
 	}
