61ff0525a4ff99178cc934fb9ac81ffad3b84189
Nick Wang <nicholas.wang.h@gmail.com>
2020-06-22 10:54:46 +1000

    Implement reader

--- routes/reader.js
+++ routes/reader.js
@@ -2,10 +2,25 @@
 
 const router = require('express').Router(); // eslint-disable-line new-cap
 const { JSDOM } = require('jsdom');
+const htmlGen = require('../lib/htmlGen');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = (function() {
 	router.get('/reader', (req, res) => {
-		const u = req.query['url'];
+		const u = req.query['url'] || global.action['reader-url'];
+		if (u == null) {
+			res.send(htmlGen.wrap('No url'));
+		};
+
+		// set new last url
+		global.action['reader-url'] = u;
+		fs.writeFile(
+			path.join(global.mainDir, 'action.json'),
+			JSON.stringify(global.action, null, '\t'),
+			(err) => err && console.error(err)
+		);
+
 		JSDOM.fromURL(u)
 			.then((dom) => {
 				const j = new JSDOM(`<html><body></body></html>`);
@@ -13,13 +28,12 @@ module.exports = (function() {
 
 				const window = j.window;
 				const document = window.document;
-				const $ = (s) => document.querySelector(s);
 
 				document.body.appendChild(dom.window.document.querySelector('#chapter'));
 
-				$('body').style = 'background-color:#232323;color:rgba(255,255,255,0.6);';
-				$('.col-xs-12').style = `padding-left:90px;padding-right:90px;`;
-				$('#chapter-content').style = `font-family:'Times New Roman';`;
+				document.querySelector('body').style = 'background-color:#232323;color:rgba(255,255,255,0.6);';
+				document.querySelector('.col-xs-12').style = `padding-left:90px;padding-right:90px;`;
+				document.querySelector('#chapter-content').style = `font-family:'Times New Roman';`;
 
 				document.querySelectorAll('a').forEach((h) => {
 					h.style = 'color:#82A82D';
@@ -40,7 +54,6 @@ module.exports = (function() {
 				const script = document.createElement('script');
 				script.innerHTML = `
 					document.addEventListener('keydown', (event) => {
-					console.log(event.keyCode);
 					switch (event.keyCode) {
 						case 39:
 							// next page
@@ -55,8 +68,21 @@ module.exports = (function() {
 				`;
 				document.head.appendChild(script);
 
-				res.send(j.serialize());
-			});
+				let page = j.serialize();
+
+				const replace = global.action['reader-replace'];
+
+
+				for (const str in replace) {
+					if (
+						replace.hasOwnProperty(str)
+					) {
+						page = page.replace(new RegExp(str, 'g'), replace[str]);
+					}
+				}
+
+				res.send(page);
+			}).catch((e) => console.log(e));
 	});
 
 	return router;
